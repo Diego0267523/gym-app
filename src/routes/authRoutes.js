4b0dbf3e4f56c2fb71fa5d07a103ff104672401e
Diego <diegocamacho0074@gmail.com>
2026-03-25 16:13:57 -0500

    actulizacion ruta

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const verifyToken = require("../middleware/verifyToken"); // 👈 IMPORTANTE
+const verifyToken = require("../middlewares/authMiddleware"); // 👈 IMPORTANTE
 
 // ==========================
 // 🔐 AUTH
