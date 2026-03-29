97785cc2774fb115538999ee54e97a424ddfa6ab
Diego <diegocamacho0074@gmail.com>
2026-03-28 19:01:50 -0500

    API comida

--- src/controllers/aiController.js
+++ src/controllers/aiController.js
@@ -5,7 +5,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 
-const axios = require("axios");
+
 
 // 🔥 CACHE SIMPLE EN MEMORIA
 const nutritionCache = new Map();
